08db54e8b24d259d99248fbd2e2ae6c5d6b63e21
Alice Hepburn <aliceokhepburn@gmail.com>
2023-07-18 10:20:06 -0400

    ADDS seperate backend folder for json. ADDS Root.jsx and tests running website.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+import Root from './routes/Root.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    loader: async () => fetch("http://localhost:3000/verlanObject").then(response => response.json())
+
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
