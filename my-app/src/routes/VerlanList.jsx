788b4afd7b829ff080c93ea4047861d0e756a6e1
Alice Hepburn <aliceokhepburn@gmail.com>
2023-07-21 15:02:10 -0400

    ADDS search bar with functionality (in french, the english choice will currently break the site).

--- my-app/src/routes/VerlanList.jsx
+++ my-app/src/routes/VerlanList.jsx
@@ -2,25 +2,24 @@ import {useState} from 'react'
 import { useLoaderData } from 'react-router'
 import { Switch, Route, NavLink, Link } from 'react-router-dom'
 import Mot from '../Mot.jsx'
-import Alphabet from '../Alphabet.jsx'
+import Search from '../Search.jsx'
 
 export default function VerlanList() {
     const [words, setWords] = useState(useLoaderData())
     const [displayLetters, setDisplayLetters] = useState(true)
-    
-    function displayLetterLinks(){
-      setDisplayLetters(!displayLetters)
-    }
 
-  function handleLetterClick(e) {
-  const wordsToDisplay = words.filter(store => {
-    return store.name.startsWith(e.target.id);
-  })
-  }
-  const mappedWords = words.map(word=><Mot key={word.id} mot={word}/>)
+    const [searchText, setSearchText] = useState('')
+
+  const searchWords = words.filter(word =>
+  word.francais.toLowerCase().includes(searchText.toLowerCase())
+  || word.english.toLowerCase().includes(searchText.toLowerCase())
+  )
+
+  const mappedWords = searchWords.map(word=><Mot key={word.id} mot={word}/>)
 
     return (
       <div className="flex flex-col items-center w-3/4">
+        <div className="p-5"><Search searchText={searchText} setSearchText={setSearchText}/></div>
     <table className="flex flex-col items-center w-3/4">
 
       <tbody>
